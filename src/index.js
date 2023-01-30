9c4adee58b5fd0208e94d82634a140a1984cd6c8
FernandoPolanco <rafepoor@yahoo.com.co>
2023-01-30 12:51:11 -0500

    Add get

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+
 const express=require('express');
+const { json } = require('express/lib/response');
 const app=express();
 const morgan=require('morgan');
 
-//starting the server
+//setings
+app.set('port',process.env.PORT || 3000)
+app.set('json spaces',2)
+
+
+
 app.use(morgan('dev'));
+//routes
+app.use(require('./routes/index'))
+app.use('/api/movies',require('./routes/movies.Js'))
+
+// setting the app in order to allow received json data 
+app.use(express.json());
+
+// allow to received data since forms html, js and css
+app.use(express.urlencoded({extended:false}));
 
 
-app.listen(3000, ()=>{
-    console.log('Server on port ${3000}');
+// starting the server
+app.listen(app.get('port'), ()=>{
+    console.log(`Server on port ${app.get('port')}`);
 });
